836e469e6a1595bad695f6e683565d7798f6caa0
Scorhenao <samcorhenao@gmail.com>
2025-09-22 10:10:53 -0500

    Feat: add the register purchase Fix:solve 2 issues of dependencies

--- frontend/src/features/dashboard/purchases/PurchasesIndex.tsx
+++ frontend/src/features/dashboard/purchases/PurchasesIndex.tsx
@@ -6,10 +6,13 @@ import { Column, DataTable } from "../components/DataTable";
 import Modal from "../components/Modal";
 import RegisterPurchaseForm from "./components/RegisterPurchase";
 import { IPurchase } from "./Types/Purchase.type";
-import { purchases } from "./mock/purchases.mock";
+import { purchases as mockPurchases } from "./mock/purchases.mock";
 import ViewPurchase from "./components/ViewPurchase";
 
 export default function PurchasesIndex() {
+  const [purchasesData, setPurchasesData] =
+    useState<IPurchase[]>(mockPurchases);
+
   const [isRegisterModalOpen, setRegisterModalOpen] = useState(false);
   const [isDetailModalOpen, setDetailModalOpen] = useState(false);
   const [selectedPurchase, setSelectedPurchase] = useState<IPurchase | null>(
@@ -47,13 +50,20 @@ export default function PurchasesIndex() {
     },
   ];
 
+  const handleAddPurchase = (newPurchase: IPurchase) => {
+    setPurchasesData((prev) => [
+      ...prev,
+      { ...newPurchase, id: prev.length + 1 },
+    ]);
+    setRegisterModalOpen(false); // cerrar modal al guardar
+  };
   return (
     <RequireAuth>
       <div className="p-6">
         <h1 className="text-xl font-semibold mb-4">Listado de Compras</h1>
 
         <DataTable
-          data={purchases}
+          data={purchasesData}
           columns={columns}
           searchableKeys={[
             "orderNumber",
@@ -79,21 +89,9 @@ export default function PurchasesIndex() {
           title="Registrar compra"
           isOpen={isRegisterModalOpen}
           onClose={() => setRegisterModalOpen(false)}
-          footer={
-            <>
-              <button
-                onClick={() => setRegisterModalOpen(false)}
-                className="px-4 py-2 rounded-lg bg-gray-300 text-black"
-              >
-                Cancelar
-              </button>
-              <button className="px-4 py-2 rounded-lg bg-black text-white">
-                Guardar
-              </button>
-            </>
-          }
+          footer={null}
         >
-          <RegisterPurchaseForm />
+          <RegisterPurchaseForm onSave={handleAddPurchase} purchases={purchasesData}/>
         </Modal>
 
         {/* Modal Ver Detalle */}
@@ -104,7 +102,7 @@ export default function PurchasesIndex() {
           footer={
             <button
               onClick={() => setDetailModalOpen(false)}
-              className="cursor-pointer px-4 py-2 rounded-lg bg-gray-300 text-black"
+              className="cursor-pointer transition duration-300 hover:bg-gray-200 hover:text-black hover:scale-105 px-4 py-2 rounded-lg bg-gray-300 text-black"
             >
               Cerrar
             </button>
